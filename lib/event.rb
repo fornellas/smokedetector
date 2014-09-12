69b0b6e706b1a3c8f4af78b6285c666c352b9817
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2014-09-12 00:09:22 -0300

    comments

--- lib/event.rb
+++ lib/event.rb
@@ -2,21 +2,25 @@ class Event
 
   attr_accessor :raw, :type
 
+  # Receive a hash containing type: Event::Type, raw: raw event text string.
   def initialize options
     @type = options[:type]
     @raw = options[:raw]
   end
 
+  # Extracted time of event.
   def time
+#    Implement using:
 #    @raw
 #    @type.time_prefix # regex
 #    @type.time_format # DateTime.strptime
-#    @type.time_utc_offset # em segundos
   end
 
+  # Extract fields from event. Field names are returned by #type.field_names
   def [] field_name
+#    Implement using:
 #    @raw
-#    @type.fields
+#    @type.field_names
   end
 
   private
