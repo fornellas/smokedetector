2db7e07a78554a826acdff702a1c73384eca006d
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2014-09-13 12:56:42 -0300

    initial parser spec

---
+++ spec/lib/parser_spec.rb
@@ -0,0 +1,21 @@
+require 'parser'
+
+describe Parser do
+  context '#each' do
+    xit 'should throw exception if a line is longer than #max_line_bytes' do
+      
+    end
+    context 'single line events' do
+      xit 'should be able to extract events' do
+        
+      end
+    end
+    context 'multi line events' do
+      xit 'should extract events starting at lines matching #multi_line_start' do
+      end
+      xit 'should throw exception if event has more than #multi_line_max' do
+        
+      end
+    end
+  end
+end
