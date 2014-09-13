e48666c181f9d7ec5ad36a9c7005f4ad5131ad4c
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2014-09-13 17:40:30 -0300

    closes #5, closes #1

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,4 @@
+require 'pp'
 class Parser
 
   PUBLIC_ATTRS = [
@@ -9,14 +10,67 @@ class Parser
     :multi_line_max,
     ]
 
+  attr_accessor(*PUBLIC_ATTRS)
+
   def initialize options = {}
     PUBLIC_ATTRS.each do |attr|
      eval "@#{attr} = options[:#{attr}]"
     end
   end
 
-  def each
+  def each &block
+    last_line = false
+    parse_multi_line_start
+    io.each_line(max_line_length) do |line|
+      raise "Line longer than #{max_line_length}" if last_line
+      if line.length == max_line_length and line[-1, 1] != "\n"
+        last_line = true
+      end
 
+      if multi_line
+        parse_multi_line line, &block
+      else
+        parse_single_line line, &block
+      end
+    end
+  end
+
+  private
+
+  def parse_single_line line
+    yield Event.new(
+      raw: line,
+      type: event_type,
+      )
+  end
+
+  def parse_multi_line_start
+    @events = false
+    @raw = ""
+    @merge = false
+    @lines = 0
+  end
+
+  def parse_multi_line line
+    @raw += line
+    @lines += 1
+    if @lines > multi_line_max
+      raise "Event has more than #{multi_line_max} lines."
+    end
+    if line.match multi_line_start
+      @events = true
+      if @merge
+        yield Event.new(
+          raw: @raw,
+          type: event_type,
+          )
+      else
+        @merge = false
+        @raw = ""
+      end
+    else
+      raise "Invalid data before first event: '#{line}'." unless @events
+    end
   end
 
 end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,20 +1,81 @@
+require 'event'
+require 'event/type'
 require 'parser'
 
 describe Parser do
   context '#each' do
-    xit 'should throw exception if a line is longer than #max_line_lenght' do
-      
+
+    include_context 'syslog type'
+
+    let(:parser_syslog) do
+      Parser.new(
+        io: File.open('spec/support/logs/syslog'),
+        event_type: type,
+        max_line_length: 1000,
+        multi_line: false,
+        )
     end
+
+    it 'should throw exception if a line is longer than #max_line_lenght' do
+      expect do
+        parser_syslog.max_line_length = 1
+        parser_syslog.each {|e| e}
+      end.to raise_error
+    end
+
+    it 'should not throw exception if a line is not longer than #max_line_lenght' do
+      expect do
+        parser_syslog.max_line_length = 8192
+        parser_syslog.each {|e| e}
+      end.not_to raise_error
+    end
+
     context 'single line events' do
-      xit 'should be able to extract events' do
-        
+      it 'should be able to extract events' do
+        event_count = 0
+        parser_syslog.max_line_length = 8192
+        parser_syslog.each do |event|
+          event_file = "spec/support/events/syslog.#{event_count}"
+          if File.open(event_file).read != event.raw
+            raise "Event #raw does not match contents of '#{event_file}': '#{event.raw}'."
+          end
+          event_count += 1
+        end
+        if 5 != event_count
+          raise "Did not parse all events from #{spec/support/logs/syslog}."
+        end
       end
     end
+
     context 'multi line events' do
-      xit 'should extract events starting at lines matching #multi_line_start' do
+      let(:parser_multi_line) do
+        Parser.new(
+          io: File.open('spec/support/logs/multi_line'),
+          event_type: type,
+          max_line_length: 2048,
+          multi_line: true,
+          multi_line_start: /^[a-z]+ +\d+ \d{2}:\d{2}:\d{2} /i,
+          multi_line_max: 1000,
+          )
       end
-      xit 'should throw exception if event has more than #multi_line_max' do
-        
+  
+      it 'should extract events starting at lines matching #multi_line_start' do
+        parser_multi_line.multi_line_max = 1000
+        event_count = 0
+        parser_multi_line.each do |event|
+          event_file = "spec/support/events/multi_line.#{event_count}"
+          if File.open(event_file).read != event.raw
+            raise "Event #raw does not match contents of '#{event_file}': '#{event.raw}'"
+          end
+          event_count += 1
+        end
+      end
+
+      it 'should throw exception if event has more than #multi_line_max' do
+        parser_multi_line.multi_line_max = 2
+        expect do
+          parser_multi_line.each {|event| }
+        end.to raise_error
       end
     end
   end
