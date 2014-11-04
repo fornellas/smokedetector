2cb7547c29786f9da95bbe6af1b233e9c8ed3714
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2014-11-04 00:39:03 -0200

    Finished graph headers.

--- lib/graph.rb
+++ lib/graph.rb
@@ -11,34 +11,48 @@ class Graph
   def fprint io
     @io = io
     @terminal_height, @terminal_width = io.winsize
+    @buffer = ''
     compact_areas
     # Header
     horiz_divider
-    printf @io, "|#{@report[0,0]}#{' '*(label_width-@report[0, 0].size)}"
-    printf @io, "|#{area_names}#{' '*(graph_width-area_names.size)}|\n"
+    to_buffer "|#{@report[0,0]}#{' '*(label_width-@report[0, 0].size)}"
+    names = area_names
+    first = names.shift
+    to_buffer "|#{first}#{' '*(graph_width-first.size)}|\n"
+    names.each do |labels|
+      to_buffer "|#{' '*label_width}|#{labels}#{' '*(graph_width-labels.size)}|\n"
+    end
     horiz_divider
     # rows
     rows
     # Scale
     horiz_divider
-    printf @io, "\n" #TODO
+    to_buffer "\n" #TODO
+    printf @io, @buffer
+    @buffer
   end
 
   private
 
+  # append giver string to @buffer
+  def to_buffer str
+    @buffer += str
+  end
+
+  # add each row to @buffer
   def rows
     (1...@report.row_count).each do |row|
-      printf @io, "|#{@report[row, 0]}#{' '*(label_width-@report[row, 0].to_s.size)}|"
+      to_buffer "|#{@report[row, 0]}#{' '*(label_width-@report[row, 0].to_s.size)}|"
       printed = 0
       (1...@report.column_count).each do |column|
         next unless @report[row,column]
         chars = ( @report[row,column] / max_graph ) * (graph_width-1)
         (0...chars).each do
           printed += 1
-          printf @io, ANSI::String.new('#').send(AREA_COLORS[column-1])
+          to_buffer ANSI::String.new('#').send(AREA_COLORS[column-1])
         end
       end
-      printf @io, "#{' '*(graph_width-printed)}|\n"
+      to_buffer "#{' '*(graph_width-printed)}|\n"
     end
   end
 
@@ -57,28 +71,50 @@ class Graph
 
   AREA_COLORS = [:green, :yellow, :blue, :magenta, :cyan, :red]
 
-  # if area_names.size are bigger than graph_width, or if number of areas is
-  # greater than AREA_COLORS.size compact areas as needed.
+  # if number of areas is greater than AREA_COLORS.size compact areas as needed.
   def compact_areas
-    unless area_names.size > graph_width \
-      or @report.column_count-1 > AREA_COLORS.size
-      return
-    end
+    return unless @report.column_count-1 > AREA_COLORS.size
     raise 'TODO'
   end
 
   def horiz_divider
-    printf @io, "+#{'-'*label_width}+#{'-'*graph_width}+\n"
+    to_buffer "+#{'-'*label_width}+#{'-'*graph_width}+\n"
   end
 
-  # return colored names of @report[0,1] up to @report[0, @report.column_count -1]
+  # return array of lines with colored names of @report[0,1] up to
+  # @report[0, @report.column_count -1]
   def area_names
-    names = ANSI::String.new("")
-    (1...@report.column_count).each do |column|
-      names += ' ' if names.size > 0
-      names += ANSI::String.new(@report[0, column]).send(AREA_COLORS[column-1])
+    lines = []
+    names_list = @report.row(0).to_a
+    names_list.shift
+    color = 0
+    until names_list.empty?
+      line_str = ANSI::String.new("")
+      while line_str.size < graph_width
+        break if names_list.empty?
+        extra_space = line_str.text.empty? ? 0 : 1
+        if line_str.size + extra_space + names_list.first.size <= graph_width
+          colored_name = ANSI::String.new(names_list.shift).send(AREA_COLORS[color])
+          color += 1
+          line_str += ANSI::String.new("") + ( ' ' * extra_space ) + colored_name
+        else
+          name = names_list.shift
+          split_point = graph_width - (line_str.size + extra_space)
+          if split_point == 0
+            names_list.unshift name
+            break
+          else
+            first_half = name.slice(0, split_point)
+            second_half = name.slice(split_point)
+            colored_name = ANSI::String.new(first_half).send(AREA_COLORS[color])
+            line_str += ANSI::String.new("") + ( ' ' * extra_space ) + colored_name
+            names_list.unshift(second_half)
+          end
+        end
+      end
+      lines << line_str
     end
-    names
+    lines
   end
 
   # return width of @report[*, 0]
@@ -94,6 +130,8 @@ class Graph
   # return width of graph area
   def graph_width
     borders = 3
-    @terminal_width - ( borders + label_width )
+    width = @terminal_width - ( borders + label_width )
+    raise 'Terminal width too small.' if width < 2
+    width
   end
 end
--- spec/lib/graph_spec.rb
+++ spec/lib/graph_spec.rb
@@ -1,3 +1,6 @@
+require 'query'
+require 'parser'
+require 'report'
 require 'graph'
 require 'matrix'
 
@@ -9,48 +12,93 @@ describe Graph do
     before(:example) do
       query = Query.new parser_http
       @report = Report.new(query)
+      @io = instance_double('IO')
+      allow(@io).to receive(:write)
     end
 
-    it 'works with x as strings' do
-      report = ['minimum', 'response_time/url', 'by', 'minute']
-      graph = Graph.new @report.where(report)
-      graph.fprint STDERR
-    end
-
-    xit 'works with x as numbers' do
-
-    end
-
-    xit 'works with x as time' do
-
-    end
-
-    xit "compacts report to fit terminal size" do
-
-    end
-
-    xit 'works with single column reports' do
-
-    end
-
-    xit 'works with multiple columns reports' do
-
-    end
-
-    xit 'should throw exeption if terminal width is small' do
-
-    end
-
-    xit 'should throw exeption if terminal height is small' do
+    xit 'should return the same string sent to io' do
 
     end
 
     xit 'should throw exception if io is not a terminal' do
-      
+
     end
 
-    xit 'works with negative numbers' do |variable|
-      
+    context 'header' do
+
+      before(:example) do
+        report = ['count', 'events/status', 'by', 'field', 'url']
+        @graph = Graph.new @report.where(report)
+      end
+
+      it "should throw exception if terminal is too narrow" do
+        allow(@io).to receive(:winsize).and_return([20, 7])
+        expect{ @graph.fprint @io }.to raise_error
+      end
+
+      it "shouhld not throw expeption if terminal is wide enough" do
+        allow(@io).to receive(:winsize).and_return([20, 8])
+        expect{ @graph.fprint @io }.not_to raise_error
+      end
+
+      it "should print labels in single line if terminal is big enough" do
+        allow(@io).to receive(:winsize).and_return([20, 13])
+        str = @graph.fprint @io
+        header = str.split("\n").first(3).join("\n")+"\n"
+        expect(header).to eq("+---+-------+\n|url|\e[32m200\e[0m \e[33m500\e[0m|\n+---+-------+\n")
+      end
+
+      context 'multi line area names' do
+        it "shoult work breaking label name in half" do
+          allow(@io).to receive(:winsize).and_return([20, 12])
+          str = @graph.fprint @io
+          header = str.split("\n").first(4).join("\n")+"\n"
+          expect(header).to eq("+---+------+\n|url|\e[32m200\e[0m \e[33m50\e[0m|\n|   |\e[33m0\e[0m     |\n+---+------+\n")
+        end
+        it 'should work without breaking label name in half' do |variable|
+          allow(@io).to receive(:winsize).and_return([20, 10])
+          str = @graph.fprint @io
+          header = str.split("\n").first(4).join("\n")+"\n"
+          expect(header).to eq("+---+----+\n|url|\e[32m200\e[0m |\n|   |\e[33m500\e[0m |\n+---+----+\n")
+        end
+      end
+
+    end
+
+    context 'columns' do
+      xit "consolidate columns if there are too many" do
+
+      end
+
+      xit 'works with single column reports' do
+
+      end
+
+      xit 'works with multiple columns reports' do
+
+      end
+    end
+
+    context 'rows' do
+      xit 'should throw exeption if terminal height is small' do
+
+      end
+
+      xit 'works with x as strings' do
+
+      end
+
+      xit 'works with x as numbers' do
+
+      end
+
+      xit 'works with x as time' do
+
+      end
+
+      xit 'works with negative numbers' do
+
+      end
     end
 
   end
