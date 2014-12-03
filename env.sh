a89d627b5b24b9f13d46578b5303323965c93bbf
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2014-12-03 00:22:41 -0200

    Fixed relative to absolute path

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
-export GEM_HOME="$(dirname "$0")/.gem_home"
-PATH="/opt/ruby21/bin:$(dirname "$0")/bin:$GEM_HOME/bin:$PATH"
+PREFIX="$(realpath "$(dirname "$0")")"
+export GEM_HOME="$PREFIX/.gem_home"
+PATH="/opt/ruby21/bin:$PREDIX/bin:$GEM_HOME/bin:$PATH"
