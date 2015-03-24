54ba62d03527d1cdc00121efebacd2b8e7c7f7fe
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2015-03-23 23:03:20 -0300

    Graphing progress

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,21 +1,144 @@
 #require 'io/console'
 require 'report/data'
+require 'ansi/string'
 
 class Graph
 
   # Receive Report::Data object.
   def initialize report_data
-    @report_data = report_data
+    @original_report_data = report_data
   end
+
+  # Raised when terminal is too small
+  class SmallTerminal < RuntimeError ; end
 
   # Write graph to given IO object, which must respond to #winsize.
   def fprint io
-    @terminal_height, @terminal_width = io.winsize
-    
+    @io = io
+    get_terminal_attrs
+    max_label_width.downto(min_label_width).each do |label_width|
+      new_graph(label_width)
+#      compact_areas
+#      draw_header
+#      compact_rows
+#      draw_rows
+      # Retry if label_width was too big
+      if @biggest_label_width < @label_width and @label_width > min_label_width
+        next
+      end
+#      draw_graph_legend
+    end
+    printf @io, @buffer
   end
 
   private
 
+  # ----------------------------------------
+  # :section: Terminal
+  # ----------------------------------------
 
+  # Read terminal attributes and save to @terminal_height, @terminal_width
+  def get_terminal_attrs
+    @terminal_height, @terminal_width = @io.winsize
+    if @terminal_width < min_terminal_width
+      raise SmallTerminal.new("Terminal width (#{@terminal_width}) is too small, must be at least #{min_terminal_width}.")
+    end
+  end
+
+  # ----------------------------------------
+  # :section: Drawing functions
+  # ----------------------------------------
+
+  # Terminal colors for each graph area
+  AREA_COLORS = [:magenta, :blue, :cyan, :green, :yellow, :red]
+
+  # Set variables for new graph with given label width
+  def new_graph label_width
+    @label_width = label_width
+    @report_data = @original_report_data.dup # Work on a copy
+    @buffer = ''
+    @biggest_label_width = 0
+  end
+
+  # Draw headers based on @label_width
+  def draw_header
+    horiz_divider
+    @buffer += "|#{label(@report_data.matrix[0][0])}|"
+    # TODO
+    horiz_divider
+  end
+
+  # Draw horizontal divider
+  def horiz_divider
+    @buffer += "+#{'-'*@label_width}+#{'-'*graph_width}+\n"
+  end
+
+  # Convert str to a String that fit inside @label_width, trimming big strings.
+  # Update @biggest_label_width to str.size if it is bigger than @biggest_label_width.
+  def label str
+    size = str.size
+    @biggest_label_width = size if size > @biggest_label_width
+    if size > @label_width
+      "#{str[0,@label_width-3]}..."
+    else
+      "#{str}#{' ' * (@label_width - size)}"
+    end
+  end
+
+  # ----------------------------------------
+  # :section: @report_data processing
+  # ----------------------------------------
+
+  # Compact @report_data to fit #max_areas.
+  def compact_areas
+    report_areas = @report_data.matrix[0].size - 1
+    return unless report_areas > max_areas
+    lines = []
+    header = @report_data.matrix[0][(0...max_areas)]
+    header << 'others'
+    lines << header
+    row_count = @report_data.matrix.size
+    (1...row_count).each do |row_number|
+      row = @report_data.matrix[row_number]
+      others = row[max_areas..row.size].inject(:+)
+      lines << ( row[0...max_areas] << others )
+    end
+    @report_data = Report::Data.new(
+      matrix: [*lines],
+      type: @report_data.type,
+      size: @report_data.size,
+      )
+  end
+
+  # Maximum number or graph areas
+  def max_areas
+    AREA_COLORS.size
+  end
+
+  # ----------------------------------------
+  # :section: Table dimensions
+  # ----------------------------------------
+
+  # Vertical separators width
+  def vert_sep_width ; 3 ; end
+
+  # Maximum label width set to half terminal width
+  def max_label_width ; ( @terminal_width - vert_sep_width ) / 2 ; end
+
+  # Minimum width for label column (eg. label equal "L...")
+  def min_label_width ; 4 ; end
+
+  # Graph column width, based on current @label_width
+  def graph_width ; @terminal_width - @label_width - vert_sep_width ; end
+
+  # Minimum graph column width (eg: "#|#")
+  def min_graph_width ; 3 ; end
+
+  # Minimum terminal width
+  def min_terminal_width 
+    3 + # borders
+    min_label_width +
+    min_graph_width
+  end
 
 end
--- spec/lib/graph_spec.rb
+++ spec/lib/graph_spec.rb
@@ -26,7 +26,8 @@ describe Graph do
       end.to raise_error(Errno::ENOTTY)
     end
 
-    it 'throws exception if terminal is small'
+    it 'throws exception if terminal is narrow'
+    it 'throws exception if terminal is short'
     it 'works with all positive numbers'
     it 'works with all negative numbers'
     it 'works with positive and negative numbers'
@@ -34,16 +35,133 @@ describe Graph do
     it 'compacts rows with type :partition'
     it 'compacts rows with type :time'
 
-    # context 'STDERR IO test' do
+    xcontext 'STDERR IO test' do
 
-    #   example "sample", :focus do
+      example "sample", :focus do
 
-    #     allow($stderr).to receive(:winsize).and_return([24, 80])
-    #     puts
-    #     graph.fprint $stderr
-    #     puts
-    #     pending
-    #   end
-    # end
+        allow($stderr).to receive(:winsize).and_return([24, 80])
+        puts
+        graph.fprint $stderr
+        puts
+        pending 'just a test'
+      end
+    end
+  end
+
+  context 'Private instance method' do
+
+    include_context 'nginx type'
+    include_context 'parser nginx'
+
+    let(:graph) do
+      query = Query.new parser_nginx
+      report = Report.new(query)
+      stat_by_bucket = ['count', 'events/http_user_agent', 'by', 'hour']
+      Graph.new(report.where(stat_by_bucket))
+    end
+
+    context '#get_terminal_attrs' do
+      before(:example) do
+        console_double = double("IO", winsize: [24, 80])
+        graph.instance_eval{@io = console_double}
+      end
+      it 'saves terminal data to @terminal_height, @terminal_width' do
+        graph.instance_eval{get_terminal_attrs}
+        expect(graph.instance_eval{[@terminal_height, @terminal_width]})
+          .to eq([24, 80])
+      end
+
+      it 'raises if width is smaller than #min_terminal_width' do
+        allow(graph).to receive(:min_terminal_width).and_return(81)
+        expect{graph.instance_eval{get_terminal_attrs}}.to raise_error(Graph::SmallTerminal)
+      end
+    end
+
+    context '#draw_header' do
+      it 'draws single line header'
+      it 'draws multi line headers'
+    end
+
+    context '#horiz_divider' do
+      before(:example) do
+        console_double = double("IO", winsize: [10, 10])
+        graph.instance_eval do
+          @io = console_double
+          @buffer = ''
+          @label_width = 4
+          get_terminal_attrs
+        end
+      end
+      it 'draws a horizontal divider' do
+        graph.instance_eval{horiz_divider}
+        expect(graph.instance_eval{@buffer}).to eq("+----+---+\n")
+      end
+    end
+
+    context '#label' do
+      before(:example) do
+        graph.instance_eval do
+          @label_width = 5
+          @biggest_label_width = 0
+        end
+      end
+      it 'trim big strings to fit @label_width' do
+        expect(graph.instance_eval{label "123456"}).to be == "12..."
+      end
+
+      it 'add spaces to short strings to fill @label_width' do
+        expect(graph.instance_eval{label "1"}).to be == "1    "
+      end
+      it 'updates @biggest_label_width if str is bigger' do
+        expect{graph.instance_eval{label "12345"}}
+          .to change{graph.instance_eval{@biggest_label_width}}
+          .from(0).to(5)
+      end
+      it 'does not update @biggest_label_width if str is smaller' do
+        graph.instance_eval{@biggest_label_width = 5}
+        expect{graph.instance_eval{label "12345"}}
+          .not_to change{graph.instance_eval{@biggest_label_width}}
+      end
+    end
+
+    context '#compact_areas' do
+      # TODO
+      it 'compacts @report_data if it has more areas than #max_areas'
+      it 'does not compact @report_data if it has less or equal areas than #max_areas'
+    end
+
+    context '#max_areas' do
+      it 'returns the number of AREA_COLORS' do
+        number_of_area_colors = graph.instance_eval{Graph::AREA_COLORS.size}
+        expect(graph.instance_eval{max_areas}).to eq(number_of_area_colors)
+      end
+    end
+
+    context '#max_label_width' do
+      before(:example) do
+        console_double = double("IO", winsize: [24, 80])
+        graph.instance_eval do
+          @io = console_double
+          @buffer = ''
+          get_terminal_attrs
+        end
+      end
+      it 'returns half terminal size' do
+        expect(graph.instance_eval{max_label_width}).to eq(38)
+      end
+    end
+
+    context '#min_label_width' do
+      it 'returns size of "L..."' do
+        expect(graph.instance_eval{min_label_width}).to eq("L...".size)
+      end
+    end
+
+    context '#min_graph_width' do
+      it 'returns size of "#|#"' do
+        expect(graph.instance_eval{min_graph_width}).to eq("#|#".size)
+      end
+    end
+
   end
 end
